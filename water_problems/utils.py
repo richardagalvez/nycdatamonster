65fc6bbfe3b7df39d5eb0d0fbd4c00d0a1e3ef76
richardagalvez <richardagalvez@gmail.com>
2018-04-02 15:24:16 -0400

    updated utils.py for water problems

---
+++ water_problems/utils.py
@@ -0,0 +1,17 @@
+from geopy.geocoders import Nominatim
+import numpy as np
+
+geolocator = Nominatim()
+
+def lat_long(address):
+    location = geolocator.geocode(address)
+    try:
+        return location.latitude, location.longitude
+    except:
+        print('could not find address: ', address)
+        return np.nan, np.nan
+
+# notes
+
+# df['violation_description'] = df.violation_description.str.replace("'",'')
+# df['dba'] = df.dba.str.replace("'",'')
